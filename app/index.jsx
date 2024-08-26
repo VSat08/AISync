518599f2564f211902db575fce374fb84fb9cd64
VSat08 <satyam2001verma@gmail.com>
2024-08-26 20:30:14 +0530

    Integrating fonts, colors and icons

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl text-center">AISync</Text>
+      <Text className="text-3xl text-center font-pmedium">AISync</Text>
       <Text className="text-base text-center">React Native Development</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "red" }}>
