8eeecc4361f140a343ed9f19ade80d20b5fc8f7d
VSat08 <satyam2001verma@gmail.com>
2024-08-30 22:51:52 +0530

    Shifting from React Native SDK for appwrite to appwrite(version inconsistencies) and creating vodeo frames

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,16 +1,106 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+import { icons } from "../constants";
+import { ResizeMode, Video } from "expo-av";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1.1,
+  },
+};
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ item, activeItem }) => {
+  const [play, setPlay] = useState(false);
+  // console.log(item.video);
+  return (
+    <Animatable.View
+      className="mx-3"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={300}
+    >
+      {play ? (
+        <Video
+          source={{
+            uri: item.video,
+          }}
+          className="w-52 h-72 rounded-3xl mt-3 bg-white/10"
+          resizeMode={ResizeMode.COVER}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
+      ) : (
+        <TouchableOpacity
+          className="relative flex justify-center items-center "
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            source={{
+              uri: item.thumbnail,
+            }}
+            className="w-52 h-72 rounded-3xl my-5 overflow-hidden shadow-2xl shadow-white/40"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="h-16 w-16 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-white text-2xl">{item.id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{
+        x: 170,
+      }}
       horizontal
-    ></FlatList>
+    />
   );
 };
 
